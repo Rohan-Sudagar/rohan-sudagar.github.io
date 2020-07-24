fc949a13aedc17271672030b430098cf01583e6b
Rohan-Sudagar <66852579+Rohan-Sudagar@users.noreply.github.com>
2020-07-23 15:17:45 -1000

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,15 @@
 <?php
-$con = mysql_connect('localhost:8889', 'root', 'root')
-if(!$con){
-    echo 'Not Connected To Server';
-}
-if(!mysqli_select_db($con,'Hotel_Booking_Database'))
-{
-    echo' Not Connected To db';
-}
 
-$NAME = $_POST['name'];
-$EMAIL = $_POST['email'];
-$FROM_DATE = $_POST['From_Date'];
-$TO_DATE = $_POST['To_Date'];
+
+$NAME = $_GET['name'];
+$EMAIL = $_GET['email'];
+$FROM_DATE = $_GET['From_Date'];
+$TO_DATE = $_GET['To_Date'];
 $ROOM_NUMBER = 763;
-$ROOM_SERVICE = $_POST['Room_Service'];
+$ROOM_SERVICE = $_GET['Room_Service'];
+$time = $_GET['time'];
 
-$sql = "INSERT INTO Hotel_Reservations(Name, Email, From_Date, To_Date, Room_Number, Room_Service) Values('$NAME', '$EMAIL','$FROM_DATE', '$TO_DATE', $ROOM_NUMBER, '$ROOM_SERVICE')";
 
-if(!mysqli_query($con,$sql))
-{
-    $msg = "Room Not Confirmed";
-}
-else{
-    $msg = "Room Confirmed. Please Come To 1420 South Rug St For Your Room";
-
-    require_once 'PHPMailerAutoload.php';
 
 
 $mail = new PHPMailer();
@@ -38,10 +23,45 @@ $mail->Username = 'rohansudagar@gmail.com';
 $mail->Password = 'tuesday2012';
 $mail->SetFrom('rohansudagar@gmail.com');
 $mail->Subject = 'Your Hotel Stay';
-$mail->Body = 'You Confirmed A Stay At Rohan Hotel At <a href="https://rohan-sudagar.github.io">https://rohan-sudagar.github.io</a> Please Come To 1420 South Rug St For Your Room ';
+$mail->Body = 'You Confirmed A Stay At Rohan Hotel At <a href="https://rohan-sudagar.github.io">https://rohan-sudagar.github.io</a> Please Come To 1420 South Rug St For Your Room 
+<br>
+The date you chose was '. $FROM_DATE .'.
+<br>
+The time you chose was '. $time .'. 
+<br>
+Your room number is ' .$ROOM_NUMBER. '.
+<br>
+The choice you chose was ' .$ROOM_SERVICE. '.';
 
-$mail->AddAddress($_POST['email']);
+$mail->AddAddress($EMAIL);
 $mail->Send();
     
 }
+?>
+
+<?php
+ require_once 'PHPMailerAutoload.php';
+
+
+$mail = new PHPMailer();
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'ssl';
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = '465';
+$mail->IsHTML(true);
+$mail->Username = 'rohansudagar@gmail.com';
+$mail->Password = 'tuesday2012';
+$mail->SetFrom('rohansudagar@gmail.com');
+$mail->Subject = 'Your Hotel Stay';
+$mail->Body = 'A customer wants to come to your hotel. Your customers name is ' .$NAME. '.
+<br>
+The time your customer wants to come is '. $time .'
+<br>
+The date your customer wants to come in is '. $FROM_DATE .'.
+    <br>
+    The date your customer wants to leave is '. $TO_DATE .'
+;
+$mail->AddAddress('rohansudagar@gmail.com');
+$mail->Send();
 ?>
